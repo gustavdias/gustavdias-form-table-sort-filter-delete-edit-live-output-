b009010ddae03fa8bb9bfbbb98d4d0737aa26900
gustavdias <gustav.almd@gmail.com>
2021-02-17 21:57:14 +0300

    first commit

---
+++ my-basic-react-form/src/InlineForm.js
@@ -0,0 +1,91 @@
+import React from "react";
+import {TextField} from "@material-ui/core";
+import {Button} from "@material-ui/core";
+import {Check} from "@material-ui/icons";
+import {Cancel} from "@material-ui/icons";
+import { TableRow, TableCell } from "@material-ui/core";
+
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: {
+        ...props.x
+      },
+      errors: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        username: ""
+      }
+    };
+  }
+
+  change = e => {
+    const { name, value } = e.target;
+    this.setState(state => ({
+      values: {
+        ...state.values,
+        [name]: value
+      }
+    }));
+  };
+
+  validate = () => {
+    let isError = false;
+    const errors = {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: ""
+    };
+
+    const { username, email } = this.state.values;
+
+    if (username.length < 5) {
+      isError = true;
+      errors.username = "Username needs to be atleast 5 characters long";
+    }
+
+    if (email.indexOf("@") === -1) {
+      isError = true;
+      errors.email = "Requires valid email";
+    }
+
+    this.setState({
+      errors
+    });
+
+    return isError;
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const err = this.validate();
+    if (!err) {
+      this.props.handleSave(this.props.i, this.state.values);
+    }
+  };
+
+  render() {
+    const { header, x, i } = this.props;
+    return [
+      header.map((y, k) => (
+        <TableCell key={`trc-${k}`}>
+          <TextField
+            name={y.prop}
+            onChange={this.change}
+            value={this.state.values[y.prop]}
+            errorText={this.state.errors[y.prop]}
+          />
+        </TableCell>
+      )),
+      <TableCell key="icon-row-column">
+        <Check onClick={this.onSubmit} />
+        <Cancel onClick={this.props.stopEditing} />
+      </TableCell>
+    ];
+  }
+}
